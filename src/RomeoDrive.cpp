145196572a30fbed9fb14271467f018404222df4
Mr. J.A.Bianchi <MisterBianchi@gmail.com>
2019-01-01 19:17:23 -0500

    dereferenced usage of RomeoMotor objects

--- src/RomeoDrive.cpp
+++ src/RomeoDrive.cpp
@@ -27,44 +27,44 @@ RomeoDrive::RomeoDrive()
 
 void RomeoDrive::arcade(){
   
-  int turnSpeed = _phone.axis(X);
-  int moveSpeed = _phone.axis(Y);
+  int turnSpeed = *_phone.axis(X);
+  int moveSpeed = *_phone.axis(Y);
 
-  _rightMotor.move(moveSpeed - turnSpeed);
-  _leftMotor.move(moveSpeed + turnSpeed);
+  *_rightMotor.move(moveSpeed - turnSpeed);
+  *_leftMotor.move(moveSpeed + turnSpeed);
   
 }
 
 void RomeoDrive::arcade(int moveSpeed, int turnSpeed){
   
-  _rightMotor.move(moveSpeed - turnSpeed);
-  _leftMotor.move(moveSpeed + turnSpeed);
+  *_rightMotor.move(moveSpeed - turnSpeed);
+  *_leftMotor.move(moveSpeed + turnSpeed);
   
 }
 
 
 void RomeoDrive::tank(){
   
-  int moveRightSpeed = _phone.axis(Y);
-  int moveLeftSpeed = _phone.axis(Y);   //new axis needs to be added when double joystick app is functional
+  int moveRightSpeed = *_phone.axis(Y);
+  int moveLeftSpeed = *_phone.axis(Y);   //new axis needs to be added when double joystick app is functional
 
-  _rightMotor.move(moveRightSpeed);
-  _leftMotor.move(moveLeftSpeed);
+  *_rightMotor.move(moveRightSpeed);
+  *_leftMotor.move(moveLeftSpeed);
   
 }
 
 void RomeoDrive::tank(int moveRightSpeed, int moveLeftSpeed){
 
-  _rightMotor.move(moveRightSpeed);
-  _leftMotor.move(moveLeftSpeed);
+  *_rightMotor.move(moveRightSpeed);
+  *_leftMotor.move(moveLeftSpeed);
 
 }
 
 
 void RomeoDrive::end(){
 
-  _rightMotor.end();
-  _leftMotor.end();
+  *_rightMotor.end();
+  *_leftMotor.end();
 
 }
 
