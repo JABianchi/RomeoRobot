e7c0ac9d6acb9ef994d9c3ba8c96434af35a8b16
Mr. Bianchi <MisterBianchi@gmail.com>
2019-11-29 15:21:33 -0500

    added RomeoServobot.h

--- src/RomeoRobot.h
+++ src/RomeoRobot.h
@@ -2,16 +2,13 @@
  * @file RomeoRobot.h
  * @brief  Libraries for using RomeoBLE
  * @author Joel Andrew Bianchi
- * @date December 23, 2018.
+ * @date November 29, 2019.
  * @license MIT License (MIT) Released into the public domain.
  */
 
 #pragma once
 
-//#include <RomeoMotor.h>
-//#include <RomeoJoystick.h>
-//#include <RomeoDrive.h>
-
 #include "RomeoMotor.h"
 #include "RomeoJoystick.h"
 #include "RomeoDrive.h"
+#include "RomeoServobot.h"
